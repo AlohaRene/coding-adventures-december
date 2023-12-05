e3d3e1c7ab4cb679adb253caaec503ec5f3ad05c
Rene Arenas Flores <43974861+AlohaRene@users.noreply.github.com>
2023-12-04 22:05:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-JavaScript code goes here
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  // This code will execute after the DOM (HTML) has been fully loaded
+  
+  // Example: Changing text when a button is clicked
+  const changeTextButton = document.getElementById('changeTextButton');
+  const textToChange = document.getElementById('textToChange');
+
+  changeTextButton.addEventListener('click', function() {
+    textToChange.textContent = 'Text Changed!';
+  });
+});
