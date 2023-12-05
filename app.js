a542e29567348364f3600bd318dfb59e52789f34
Rene Arenas Flores <reneonlineg1@gmail.com>
2023-12-05 03:41:35 -0500

    CHORE  add MORE HTML CSS JS git statusgit statusgit status  :p (Ticket-2)

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
-  // This code will execute after the DOM (HTML) has been fully loaded
+  const countdownElement = document.getElementById('countdown');
+  const startCountdownButton = document.getElementById('startCountdownButton');
+  let count = 10;
   
-  // Example: Changing text when a button is clicked
-  const changeTextButton = document.getElementById('changeTextButton');
-  const textToChange = document.getElementById('textToChange');
+  startCountdownButton.addEventListener('click', function() {
+    const countdownInterval = setInterval(function() {
+      countdownElement.textContent = count;
+      
+      // Change button color every second
+      startCountdownButton.style.backgroundColor = getRandomColor();
 
-  changeTextButton.addEventListener('click', function() {
-    textToChange.textContent = 'Text Changed!';
+      count--;
+      if (count < 0) {
+        clearInterval(countdownInterval);
+        countdownElement.textContent = 'Countdown Complete!';
+      }
+    }, 1000);
   });
+
+  function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 });
